771df1cb8d13e0b3a98715918705bfcaea9830cd
Logan <logknees@github.com>
2019-01-16 13:44:31 -0600

    Started on bottom add/search functionality

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 		<div class="row">
 			&nbsp;
 		</div>
-		<div class="row">
+		<!--<div class="row">
 			<div class="col">
 				<p>Search:</p>
 			</div>
@@ -45,30 +45,50 @@
 			<div class="col">
 				<button name="data" type="button" id="submit">Go!</button>
 			</div>
-		</div>
+		</div>-->
 	</div>
 	<div id="results">
 
 
 	</div>
 	<div class="loader"></div>
+	<div class="container">
+		<div class="footer fixed-bottom lp-footer rounded">
+			<div class="row">
+				<div class="col"><label for="website">Website</label></div>
+				<div class="col"><label for="DA">DA</label></div>
+				<div class="col"><label for="names">Names</label></div>
+				<div class="col"><label for="title">Titles</label></div>
+				<div class="col"><label for="email">Email</label></div>
+				<div class="col"><label for="phone">Phone</label></div>
+				<div class="col"><label for="status">Status</label></div>
+				<div class="col"><label for="last_poc">Last POC</label></div>
+				<div class="col"><label for="times_contacted">Times Contacted</label></div>
 
-	<div class="footer fixed-bottom">
-		<div class="row">
-			<div class="col">
-				test
+				<div class="col"><label for="notes">Notes</label></div>
+				<div class="col"><label for="signed">Signed?</label></div>
+				<div class="col"><label for="publishing_coordinator">Coordinator</label>
+				</div>
 			</div>
-			<div class="col">
-				test
-			</div>
-			<div class="col">
-				test
-			</div>
-			<div class="col">
-				test
-			</div>
-			<div class="col">
-				test
+			<div class="row">
+				<div class="col"><input type="text" class="form-control" name="website" id="website"></div>
+				<div class="col"><input type="text" class="form-control" name="DA"></div>
+				<div class="col"><input type="text" class="form-control" name="names" id="names"></div>
+				<div class="col"><input type="text" class="form-control" name="title"></div>
+				<div class="col"><input type="text" class="form-control" name="email" id="email"></div>
+				<div class="col"><input type="text" class="form-control" name="phone" id="phone"></div>
+				<div class="col"><input type="text" class="form-control" name="status"></div>
+				<div class="col"><input type="text" class="form-control" name="last_poc"></div>
+				<div class="col"><input type="text" class="form-control" name="times_contacted"></div>
+				<div class="col"><input type="text" class="form-control" name="notes"></div>
+				<div class="col"><input type="checkbox" class="form-control" name="signed"></div>
+				<div class="col">
+					<select class="form-control" name="publishing_coordinator">
+						<option value="1">Emmy</option>
+						<option value="2">Mallory</option>
+						<option value="3">Morgan</option>
+					</select>
+				</div>
 			</div>
 		</div>
 	</div>
--- submitSearch.php
+++ submitSearch.php
@@ -32,4 +32,37 @@ $(document).ready(function(){
 
 });
 
+$(document).ready(function(){
+	
+	$("#website, #names, #email, #phone").on("keyup", function(){
+		if(this.value.length > 2){
+			console.log('hi');
+		}
+		/*$('.loader').toggle();
+		var searchField = $('#searchBy').val();
+		var searchData = $('#searchData').val();
+		var sortData = $('#sortData').val();
+		var sortOrder = $('#sortOrder').val();
+		$.ajax({
+			type: "POST",
+			url: 'search.php',
+			data: {
+				searchField: searchField,
+				searchData: searchData,
+				sortData: sortData,
+				sortOrder: sortOrder
+			},
+			success: function(data)
+			{
+			    append(JSON.parse(data));
+			    $('.loader').toggle();
+			},
+			  error: function(XMLHttpRequest, textStatus, errorThrown) {
+			     alert(errorThrown);
+			  }
+		});*/
+	});
+
+});
+
 </script>
